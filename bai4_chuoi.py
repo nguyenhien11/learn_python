71b6d9b11c7ebbd21f1c09a22b51ba5130c7b318
nguyenhien <nguyenhien112002@gmail.com>
2023-02-20 23:51:00 +0700

    Create bai4_chuoi.py
    bai4_chuoi
    https://toidicode.com/chuoi-trong-python-344.html

---
+++ bai4_chuoi.py
@@ -0,0 +1,17 @@
+print("Website \"Toidicode.com\" ")
+
+print('Website \'Toidicode.com\' ')
+
+guy = "Ban"
+
+full = "Chao mung %s den voi python" %(guy)
+print(full)
+
+
+name = "Nguyen Ngoc Hien"
+print(name[0])
+print(name[12])
+
+print(name[0:6])
+print(name[7:11])
+print(name[12:16])
